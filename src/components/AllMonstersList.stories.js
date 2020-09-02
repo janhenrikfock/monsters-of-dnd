da1d7c97b99ccb159139b33ab86efa3c3e19d6b3
janhenrikfock <jan.henrik.fock@googlemail.com>
2020-09-02 11:31:34 +0200

    Add storie allmonsterlist

---
+++ src/components/AllMonstersList.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import AllMonstersList from './AllMonstersList'
+import { LoggedIn } from '../stories/Header.stories'
+
+export default {
+  title: 'Example/AllMonstersList',
+  component: AllMonstersList,
+}
+
+export const FullList = <AllMonstersList />
